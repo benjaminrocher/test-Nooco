558d34a0ef2baa2ed640cd3f3dcc271f29cc4645
Your Name <rocher.benji@gmail.com>
2021-11-29 13:14:36 +0100

    delete

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const postsRoutes = require('./routes/postsController');
-
-app.use('/', postsRoutes)
-
-app.listen(5500, () => console.log('Server: ON \nListen: 5500'));
