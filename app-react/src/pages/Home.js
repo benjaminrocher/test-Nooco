a76665216248bc351f1bc85e7c04dd86ecf22e82
Your Name <rocher.benji@gmail.com>
2021-12-08 18:05:37 +0100

    add marker in map

--- app-react/src/pages/Home.js
+++ app-react/src/pages/Home.js
@@ -1,8 +1,32 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
 import './Home.css'
 
+function CustomMarker({marker, handleAddMarker}) {
+    const map = 
+    useMapEvents({
+        click(event) {
+          const { lat, lng } = event.latlng;
+          handleAddMarker([lat, lng]);
+        },
+      });
+    return (
+        marker.latitude !== 0 ? (
+        <Marker
+          position={marker}
+          interactive={false}
+        />
+      ) : null
+    );}
+
 const Home = () => {
+    const [markers,setMarkers] = useState([[51.505, -0.09]])
+    const handleAddMarker = (position) => {
+        markers.push(position);
+        setMarkers([...markers]);
+    }
+
+console.log(markers)
     return (
         <div className="home">
             <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
@@ -10,14 +34,11 @@ const Home = () => {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+               {markers && markers.map && markers.map((markerPosition) => <CustomMarker handleAddMarker = {handleAddMarker} marker = {markerPosition}/>)}
             </MapContainer>
         </div>
     );
+    
 };
 
 export default Home;
